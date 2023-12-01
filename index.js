39ba8dd060ba23264372c3ecb9abb6f546f3c7ea
Khanh Tran <katetrannbkhanh@gmail.com>
2023-12-01 00:39:00 -0800

    commit tree 9d5c722d416601368bd7d02a38d5265d9f040ec6

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: "Khanh",
+    streetAddress: "123 abc"
+}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj}
+    newObj[key] = value
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value
+    return obj
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newDelete = {...employee}
+    delete newDelete[key]
+    return newDelete
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
